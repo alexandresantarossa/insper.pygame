8a87a5b2e158585970ace3e2c16820ec425faa8c
Alexandre <alexandrers3@al.insper.edu.br>
2021-11-22 17:19:10 -0300

    Física feita

--- bomberman.py
+++ bomberman.py
@@ -31,23 +31,30 @@ class Player1(pygame.sprite.Sprite):
         self.rect.centerx = 0
         self.rect.bottom = HEIGHT - 10
         self.speedx = 0
+        self.speedy = 0
 
     def update(self):
-        # Atualização da posição da nave
+        # Atualização da posição do boneco
         self.rect.x += self.speedx
+        self.rect.y += self.speedy
 
         # Mantem dentro da tela
         if self.rect.right > WIDTH:
             self.rect.right = WIDTH
         if self.rect.left < 0:
             self.rect.left = 0
+        if self.rect.top < 0:
+            self.rect.top = 0
+        if self.rect.bottom > HEIGHT:
+            self.rect.bottom = HEIGHT
+
 
 game = True
 # Variável para o ajuste de velocidade
 clock = pygame.time.Clock()
 FPS = 30
 
-# Criando um grupo de meteoros
+# Criando um grupo de sprites
 all_sprites = pygame.sprite.Group()
 # Criando o jogador
 player1 = Player1(boneco_img)
@@ -75,17 +82,28 @@ while game:
                 player1.speedx = 0
             if event.key == pygame.K_RIGHT:
                 player1.speedx = 0            
-    
+        # Verifica se apertou alguma tecla.
+        if event.type == pygame.KEYDOWN:    
+            # Dependendo da tecla, altera a velocidade.
+            if event.key == pygame.K_UP:
+                player1.speedy -= 3
+            if event.key == pygame.K_DOWN:
+                player1.speedy += 3
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_UP:
+                player1.speedy = 0
+            if event.key == pygame.K_DOWN:
+                player1.speedy = 0      
             
 
 
     # ----- Atualiza estado do jogo
-    # Atualizando a posição dos meteoros
+    # Atualizando a posição das sprites
     all_sprites.update()
 
     # ----- Gera saídas
-    window.fill((0, 255, 100))  # Preenche com a cor branca
-    # Desenhando meteoros
+    window.fill((0, 255, 100))  # Preenche com a cor verde
+    # Desenhando sprites
     all_sprites.draw(window)
 
     pygame.display.update()  # Mostra o novo frame para o jogador
